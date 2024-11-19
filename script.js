d4dccf965e1863cde32a8c8dc49f520f7f4fb33b
Harshil suthar <108947643+harris8099@users.noreply.github.com>
2024-11-19 23:48:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,23 +2,41 @@
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
+const projectLinks = {
+    "project_1": "https://github.com/harris8099/Watermark",
+    "project_2": "https://github.com/harris8099/Text-to-Morse-converter-",
+    "project_3": "https://github.com/harris8099/Type-Master",
+    "project_4": "https://github.com/harris8099/CTAE-MAP",
+    "project_5": "https://github.com/harris8099/Portfolio",
+    "project_6": "https://github.com/harris8099"
+};
 
-if(navToggle){
-    navToggle.addEventListener('click',()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     });
 }
 
-if(navClose){
-    navClose.addEventListener('click',()=>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     });
 }
 
+// projects link
+document.querySelectorAll(".project").forEach(project => {
+    project.addEventListener("click", function () {
+        const projectId = this.id;
+        const url = projectLinks[projectId];
+        window.open(url, "_blank");
+    });
+});
+
+
 // Remove menu mobile
 const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = ()=>{
+const linkAction = () => {
     navMenu.classList.remove('show-menu');
 };
 navLink.forEach(n => n.addEventListener('click', linkAction));
@@ -27,24 +45,24 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const blurHeader = () => {
     const header = document.getElementById('header');
     this.scrollY >= 50 ? header.classList.add("blur-header")
-                       : header.classList.remove("blur-header") 
+        : header.classList.remove("blur-header")
 }
 window.addEventListener("scroll", blurHeader);
 
 
 // EMAIl JS
 const contactForm = document.getElementById("contact-form"),
-contactMessage = document.getElementById('contact-message')
+    contactMessage = document.getElementById('contact-message')
 
-const sendEmail = (e) =>{
+const sendEmail = (e) => {
     e.preventDefault()
-    emailjs.sendForm('service_ixz8ryz','template_rk4oc5n','#contact-form',"YJQ0v27I9kAbXbYHK")
-    .then(()=>{
-        contactMessage.textContent = "Message sent successfully"
-        setTimeout(()=>{
-            contactMessage.textContent = ''
-        }, 5000)
-    })
+    emailjs.sendForm('service_ixz8ryz', 'template_rk4oc5n', '#contact-form', "YJQ0v27I9kAbXbYHK")
+        .then(() => {
+            contactMessage.textContent = "Message sent successfully"
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+        })
     contactForm.reset()
 }
 
@@ -53,15 +71,15 @@ contactForm.addEventListener('submit', sendEmail)
 // Breakpoints
 const widthMatch = window.matchMedia("(max-width: 1300px)");
 var w = window.innerWidth;
-if (w<=1300){
+if (w <= 1300) {
     rect(true);
 }
-function rect(x){
+function rect(x) {
     if (x) {
         document.getElementById("rect-one").classList.remove("rect_one");
         document.getElementById("rect-two").classList.remove("rect_two");
     }
-    else{
+    else {
         document.getElementById("rect-one").classList.add("rect_one");
         document.getElementById("rect-two").classList.add("rect_two");
     }
